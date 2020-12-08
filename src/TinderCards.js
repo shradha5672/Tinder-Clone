9fc83b8de689f8de388c449eb0364bf4b2bea7f5
Shradha Sharma <shradha.appvoir@gmail.com>
2020-12-08 23:45:49 +0530

    Changes in Components

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -7,10 +7,11 @@ function TinderCards() {
         {
             name:'Elon Musk',
             //url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg",
+            url:"https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
         },
         {
             name:'Jeff Bezos',
-            //url: "https://www.biography.com/.image/t_share/MTY2NzA3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+            url: "https://www.biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
         },
     ]);
     const swiped = (direction, nameToDelete) =>{
@@ -21,6 +22,7 @@ function TinderCards() {
     const outOfFrame = (name) =>{
         console.log(name + "left the Screen!");
     };
+
     return (
         <div className="tinderCards">
            <div className="tinderCards__cardContainer">
@@ -31,13 +33,21 @@ function TinderCards() {
                  preventswipe = {["up","down"]}
                  onSwipe={(dir) => swiped(dir, person.name)}
                  onCardLeftScreen = {() => outOfFrame(person.name)}
-                ></TinderCard>
+                >
+                  <div
+                  //Inline interpolation in url: person.url
+                    style = {{backgroundImage: `url(${person.url})`}}
+                    className = "card">
+                    <h3>{person.name}</h3>
+                  </div>
+                </TinderCard>
+
                 ))}
            </div>
             
             
         </div>
-    )
+    );
 }
 
 export default TinderCards
